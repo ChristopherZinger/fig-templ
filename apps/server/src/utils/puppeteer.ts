94caf3e938314d7152c144975eee5319a9b4a2f9
christopher z <krzysztof.zinger@gmail.com>
2025-10-08 06:56:51 +0200

    add puppeteer utils

---
+++ apps/server/src/utils/puppeteer.ts
@@ -0,0 +1,45 @@
+import puppeteer, { Page } from "puppeteer-core";
+
+const PUPPETEER_EXECUTABLE_PATH = process.env.PUPPETEER_EXECUTABLE_PATH;
+
+export async function withBrowserPage(
+  fn: (browser: Page) => Promise<void>
+): Promise<void> {
+  const viewport = {
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    height: 1080,
+    isLandscape: true,
+    isMobile: false,
+    width: 1920,
+  };
+
+  const browser = await puppeteer.launch({
+    executablePath: PUPPETEER_EXECUTABLE_PATH,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+    defaultViewport: viewport,
+  });
+
+  const context = await browser.createBrowserContext();
+  const page = await context.newPage();
+
+  page.setDefaultTimeout(1000 * 60);
+  page.setDefaultNavigationTimeout(1000 * 60);
+
+  try {
+    await fn(page);
+  } catch (error) {
+    console.error("failed_to_execute_function_with_browser", error);
+    throw error;
+  } finally {
+    try {
+      console.log("closing_browser");
+      await page.close();
+      await context.close();
+      await browser.close();
+    } catch (closeError) {
+      console.error("error_closing_browser", closeError);
+    }
+  }
+}
