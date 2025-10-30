66ab6c3ed5e492fd18e78fb08aa5e3f6cf2d0ca6
christopher z <krzysztof.zinger@gmail.com>
2025-10-30 08:46:39 +0100

    introduce couple layout components

---
+++ apps/plugin/src/components/BaseLayout.svelte
@@ -0,0 +1,25 @@
+<div class="base-layout">
+  <div class="content">
+    <slot name="content" />
+  </div>
+  <div class="right">
+    <slot name="sidebar" />
+  </div>
+</div>
+
+<style>
+  .base-layout {
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+  }
+
+  .content {
+    flex: 1;
+    border-right: 1px solid lightgray;
+  }
+
+  .right {
+    width: 250px;
+  }
+</style>
---
+++ apps/plugin/src/components/sidebar/SidebarItem.svelte
@@ -0,0 +1,27 @@
+<div class="sidebar-item">
+  <div class="header">
+    <slot name="header" />
+  </div>
+  <div class="content">
+    <slot name="content" />
+  </div>
+</div>
+
+<style>
+  .sidebar-item {
+    display: flex;
+    flex-direction: column;
+    padding: 0.8rem;
+  }
+
+  .header {
+    align-items: center;
+    font-size: 0.6875rem;
+    font-weight: 450;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
