aab6a0c48f19f64f62e05b89ab16f136a227a9dc
christopher z <krzysztof.zinger@gmail.com>
2025-10-30 08:50:51 +0100

    adjust plugin window size

--- apps/plugin/src/backend/index.ts
+++ apps/plugin/src/backend/index.ts
@@ -1,5 +1,5 @@
 import { setMessageRouter } from "./message-router.js";
 
-figma.showUI(__html__, { width: 600, height: 600, title: "Figma Template" });
+figma.showUI(__html__, { width: 1000, height: 1000, title: "Figma Template" });
 
 setMessageRouter(figma);
