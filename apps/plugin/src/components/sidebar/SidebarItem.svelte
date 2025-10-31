7c01fa2e787c00b1a9142966cae5cc79ff5b2385
christopher z <krzysztof.zinger@gmail.com>
2025-10-31 08:09:12 +0100

    make sidebaritem clickable

--- apps/plugin/src/components/sidebar/SidebarItem.svelte
+++ apps/plugin/src/components/sidebar/SidebarItem.svelte
@@ -1,5 +1,16 @@
+<script lang="ts">
+  export let onClick: (() => {}) | undefined = undefined;
+</script>
+
 <div class="sidebar-item">
-  <div class="header">
+  <div
+    class="header"
+    onclick={onClick}
+    onkeyup={onClick}
+    role="button"
+    tabindex="0"
+    class:button={!!onClick}
+  >
     <slot name="header" />
   </div>
   <div class="content">
@@ -11,7 +22,7 @@
   .sidebar-item {
     display: flex;
     flex-direction: column;
-    padding: 0.8rem;
+    padding: 0.7rem;
     border-bottom: 1px solid var(--light-gray);
   }
 
@@ -24,5 +35,9 @@
   .content {
     display: flex;
     flex-direction: column;
+  }
+
+  .button {
+    cursor: pointer;
   }
 </style>
