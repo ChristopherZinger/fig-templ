896928b081555d0d36d2451a1e0036c635e96a01
christopher z <krzysztof.zinger@gmail.com>
2025-11-01 18:34:07 +0100

    apply border-box style to all nodes

--- apps/plugin/src/backend/node-style-utils.ts
+++ apps/plugin/src/backend/node-style-utils.ts
@@ -14,7 +14,7 @@ export function buildNodeStyle(
 
   return [
     Object.assign(
-      {},
+      { "box-sizing": "border-box" },
       getBaseStyles(node),
       getSizeStyles(node),
       getMinMaxSizeStyles(node),
